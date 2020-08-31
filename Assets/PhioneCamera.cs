f9aa219bef432f52479859d4f72282c8c2fa540a
Noah de Bont <noahdebont@gmail.com>
2020-08-31 18:54:07 +0200

    Initial commit with camera script and aspect ratio for screen

---
+++ Assets/PhioneCamera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhioneCamera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCamera;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("no cam det");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCamera == null)
+        {
+            Debug.Log("unable to find backcamera");
+            return;
+        }
+        backCamera.Play();
+        background.texture = backCamera;
+
+        camAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)backCamera.width / (float)backCamera.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = backCamera.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCamera.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
